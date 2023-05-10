427c676f197921ce4c47ab2e04f11050e97acddd
Alex Nunzi <alex.nunzi@hotmail.it>
2023-05-10 20:10:14 +0200

    Corrette dimensioni dei font nel footer.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,11 +6,11 @@
                 @foreach($footerLinks as $list)
                     <ul class="links-list list-style-none">
                         <li>
-                            <h3 class="links-head text-white">{{ strtoupper($list['heading']) }}</h3>
+                            <h3 class="links-head text-white fs-125 fw-bold">{{ strtoupper($list['heading']) }}</h3>
                         </li>
                         @foreach($list['links'] as $link)
-                            <li>
-                                <a class="text-decoration-none terthiary-text-color fs-075" href="#">{{ $link['text'] }}</a>
+                            <li class="fs-075">
+                                <a class="text-decoration-none terthiary-text-color" href="#">{{ $link['text'] }}</a>
                             </li>
                         @endforeach
                     </ul>
@@ -26,7 +26,7 @@
             <a id="signUpBtn" class="text-white text-decoration-none" href="#SignUp">SIGN-UP NOW!</a>
             <ul class="d-flex align-center list-style-none">
                 <li class="m-3">
-                    <h3 class="secondary-text-color">FOLLOW US</h3>
+                    <h3 class="secondary-text-color fs-125 fw-bold m-0">FOLLOW US</h3>
                 </li>
                 @foreach($footerBottomLinks as $link)
                 <li class="m-2">
